c0e357989e76663b5ed60ff8a2ab91267b4d6d5c
Rodrigo Villar <rjv85@cornell.edu>
2021-07-12 22:13:23 -0400

    removed gitignore

--- quickpilot.py
+++ quickpilot.py
@@ -41,6 +41,7 @@ From Here, you can take the following actions: \n\n \
     'airport' : get information about a specific airport.\n\n\
 Additionally, you can use Control+C or 'back' to exit out of this program at any time.")
     # LATER: use library to make this text look cleaner/more professional
+    #Test
     
 if __name__ == "__main__":
     x = QuickPilot()
